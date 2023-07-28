17cf0509e47383d2090c86993daea1bf3951c7aa
Namit Aggarwal <namita@adobe.com>
2023-07-29 02:19:49 +0530

    Branch3: Add Test Case for isNotPlaindromeExceptionTest()

--- src/test/java/com/namit/StringPalindrome/AppTest.java
+++ src/test/java/com/namit/StringPalindrome/AppTest.java
@@ -8,19 +8,19 @@ public class AppTest {
 	App app = new App();
 	boolean expected = true;
 
-/*	@Test
+	@Test
 	public void isPlaindromeTest() {
 		assertEquals(expected, app.isPalindrome(input1));
-	}*/
+	}
 
 	@Test
 	public void isNotPlaindromeTest() {
 		assertEquals(false, app.isPalindrome("abc"));
 	}
 
-/*	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void isNotPlaindromeExceptionTest() {
 		assertEquals(false, app.isPalindrome(null));
-	}*/
+	}
 
 }
